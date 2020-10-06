1e080a50b1f90541a83f15bd50b4f4651ae24c09
korayguney <korayguney@sabanciuniv.edu>
2020-10-06 21:50:04 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import MyApp from "./components/MyApp";
+import MyInfo from "./components/MyInfo";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    //<React.StrictMode>
+    //  <App />
+    // </React.StrictMode>,
+    //<div><h1>Hello Koray</h1><p>This is an example paragraph</p></div>,
+    <React.StrictMode>
+        <MyInfo/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
